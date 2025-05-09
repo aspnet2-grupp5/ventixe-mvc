02e5ee8bd5674ad8a4826aef83e9acb1f0deabf4
Jessica Campbell <jesscampb91@gmail.com>
2025-05-09 11:19:18 +0200

    Created AuthService and added methods for checking if an email is already registered and for signing up a user with email and password.

---
+++ Authentication/Services/AuthService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Net.Http.Json;
+
+namespace Authentication.Services;
+
+public class AuthService(SignInManager<IdentityUser> signInManager, HttpClient httpClient)
+{
+    private readonly SignInManager<IdentityUser> _signInManager = signInManager;
+    private readonly HttpClient _httpClient = httpClient;
+
+    public async Task<bool> AlreadyExistsAsync(string email)
+    {
+        var result = await _httpClient.PostAsJsonAsync("", new { email });
+        return true;
+    }
+
+    public async Task<bool> SignUpAsync(string email, string password)
+    {
+        var result = await _httpClient.PostAsJsonAsync("", new  { email, password });
+        return true;
+    }
+}
