1be970b86a07dd0f78bc14682eb10527a5c20b9c
Jessica Campbell <jesscampb91@gmail.com>
2025-05-09 12:02:51 +0200

    Created sign up ViewModels in MVC layer.

---
+++ Ventixe.MVC/Models/SignUp/SignUpConfirmAccountViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ventixe.MVC.Models.SignUp;
+
+public class SignUpConfirmAccountViewModel
+{
+    [Required]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    public string VerificationCode { get; set; } = null!;
+}
---
+++ Ventixe.MVC/Models/SignUp/SignUpPasswordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ventixe.MVC.Models.SignUp;
+
+public class SignUpPasswordViewModel
+{
+    [Required]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password", Prompt = "Enter password")]
+    public string Password { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password", Prompt = "Confirm password")]
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
+    public string ConfirmedPassword { get; set; } = null!;
+}
