d0d68f532588059ead86a5f65599644e83c0f544
Jessica Campbell <jesscampb91@gmail.com>
2025-05-09 12:23:50 +0200

    Create email sign up base code in AuthController (MVC layer).

---
+++ Ventixe.MVC/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Authentication.Services;
+using Microsoft.AspNetCore.Mvc;
+using Ventixe.MVC.Models.SignUp;
+
+namespace Ventixe.MVC.Controllers;
+
+public class AuthController(IAuthService authService) : Controller
+{
+    private readonly IAuthService _authService = authService;
+
+    [HttpGet("signup")]
+    public IActionResult SignUpEmail()
+    {
+        return View();
+    }
+
+    [HttpPost("signup")]
+    public async Task<IActionResult> SignUpEmail(SignUpEmailViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return View(model);
+
+        if (await _authService.AlreadyExistsAsync(model.Email))
+        {
+            ViewBag.ErrorMessage = "An account already exists.";
+            return View(model);
+        }
+
+        return RedirectToAction("Verify");
+    }
+}
