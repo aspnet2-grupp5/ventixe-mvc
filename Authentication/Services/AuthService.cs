be39abca54066ba976067c7e35ea6222b63629a7
Jessica Campbell <jesscampb91@gmail.com>
2025-05-11 11:16:04 +0200

    Added try-catches and changed some variable names in AuthService.

--- Authentication/Services/AuthService.cs
+++ Authentication/Services/AuthService.cs
@@ -1,34 +1,73 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Identity;
 using System.Net.Http.Json;
 
 namespace Authentication.Services;
 
-public class AuthService(SignInManager<IdentityUser> signInManager, HttpClient httpClient) : IAuthService
+public class AuthService : IAuthService
 {
-    private readonly SignInManager<IdentityUser> _signInManager = signInManager;
-    private readonly HttpClient _httpClient = httpClient;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly HttpClient _http;
+    private readonly ServiceBusClient _client;
+    private readonly ServiceBusSender _sender;
+
+    public AuthService(SignInManager<IdentityUser> signInManager, HttpClient http, ServiceBusClient client)
+    {
+        _signInManager = signInManager;
+        _http = http;
+        _client = client;
+        _sender = _client.CreateSender("emailservice");
+    }
 
     public async Task<bool> AlreadyExistsAsync(string email)
     {
-        var result = await _httpClient.PostAsJsonAsync("", new { email });
-        return true;
+        try
+        {
+            var result = await _http.PostAsJsonAsync("", new { email });
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 
     public async Task<bool> RequestVerificationCodeAsync(string email)
     {
-        var result = await _httpClient.PostAsJsonAsync("", new { email });
-        return true;
+        try
+        {
+            var result = await _http.PostAsJsonAsync("", new { email });
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 
     public async Task<bool> ValidateVerificationCodeAsync(string email, string code)
     {
-        var result = await _httpClient.PostAsJsonAsync("", new { email, code });
-        return true;
+        try
+        {
+            var result = await _http.PostAsJsonAsync("", new { email, code });
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 
     public async Task<bool> SignUpAsync(string email, string password)
     {
-        var result = await _httpClient.PostAsJsonAsync("", new { email, password });
-        return true;
+        try
+        {
+            var result = await _http.PostAsJsonAsync("", new { email, password });
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
