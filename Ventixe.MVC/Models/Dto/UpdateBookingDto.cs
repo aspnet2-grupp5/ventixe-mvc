73e617756f2103caac2dbfa06ce442a59c75397f
May Adnan <j_may96@hotmail.com>
2025-05-12 22:31:03 +0200

    backend code for Controller / Bookings/ Dtos and Model / Status

---
+++ Ventixe.MVC/Models/Dto/UpdateBookingDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ventixe.MVC.Models.Dto
+{
+    public class UpdateBookingDto
+    {
+        public BookingStatus Status { get; set; }
+        public int Quantity { get; set; }
+        public string? EVoucher { get; set; }
+    }
+}
+
