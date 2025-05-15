326e2cc1d9b50de9a86a634b91c119ccba4b204b
Jessica Campbell <jesscampb91@gmail.com>
2025-05-15 11:30:13 +0200

    Added folder Data to Ventixe.Authentication and moved Contexts folder there.

--- Authentication/Data/Contexts/AuthenticationDbContext.cs
+++ Authentication/Data/Contexts/AuthenticationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Ventixe.Authentication.Contexts;
+namespace Ventixe.Authentication.Data.Contexts;
 
 public class AuthenticationDbContext(DbContextOptions options) : IdentityDbContext(options)
 {
--- Ventixe.MVC/Program.cs
+++ Ventixe.MVC/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Ventixe.Authentication.Contexts;
+using Ventixe.Authentication.Data.Contexts;
 using Ventixe.Authentication.Services;
 
 var builder = WebApplication.CreateBuilder(args);
