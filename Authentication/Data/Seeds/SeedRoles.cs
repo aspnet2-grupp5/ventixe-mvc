27d898b062efdf21d76abc96275a7836cdf6093d
Jessica Campbell <jesscampb91@gmail.com>
2025-06-01 16:05:04 +0200

    Created SeedRoles

---
+++ Authentication/Data/Seeds/SeedRoles.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ventixe.Authentication.Data.Seeds;
+
+public static class SeedRoles
+{
+    public static async Task SetRolesAsync(IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        string[] roles = { "Admin", "Member" };
+
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+                await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+}
