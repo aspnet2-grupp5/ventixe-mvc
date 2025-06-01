3b1ddce8339546505fbf8b59abd0097b3ea895e6
Jessica Campbell <jesscampb91@gmail.com>
2025-06-01 20:31:40 +0200

    Created UpdateProfileViewModel and CreateAccountViewModel

---
+++ Ventixe.MVC/Models/Accounts/CreateAccountViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ventixe.MVC.Models.Accounts;
+
+public class CreateAccountViewModel
+{
+    [Required]
+    public string UserId { get; set; } = null!;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
+    public string LastName { get; set; } = null!;
+
+    [Required]
+    [Phone]
+    public string Phone { get; set; } = null!;
+
+    [Required]
+    [StringLength(200)]
+    public string StreetName { get; set; } = null!;
+
+    [Required]
+    [RegularExpression(@"^\d{5}$")]
+    public string PostalCode { get; set; } = null!;
+
+    [Required]
+    [StringLength(100)]
+    public string City { get; set; } = null!;
+}
